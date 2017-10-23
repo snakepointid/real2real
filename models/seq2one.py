d1dd9209b765a274ab2fd89f502cde148dafd896
she changlue <snakepointid@sina.cn>
2017-10-23 10:08:05 +0800

    modify

--- models/seq2one.py
+++ models/seq2one.py
@@ -9,69 +9,8 @@ from real2real.modules.full_connector import final_mlp_encoder
 from real2real.utils.shape_ops import *
 
 from real2real.app.params import ctrRankModelParams,newsClsModelParams,embedLayerParams
- 
-class StackAttenCls(multiClsModel):
-            def _build_(self):
-                        # input coding placeholder
-                        self.title_source = tf.placeholder(shape=(None, newsClsModelParams.title_maxlen),dtype=tf.int64)
-                        self.content_source = tf.placeholder(shape=(None, newsClsModelParams.content_maxlen),dtype=tf.int64)
-                        self.target = tf.placeholder(shape=(None, ),dtype=tf.int32)
-                        #target to token embedding
-                        token_context = tf.get_variable('token_context',
-                                                       dtype=tf.float32,
-                                                       shape=[newsClsModelParams.target_vocab_size, embedLayerParams.embedding_dim],
-                                                       initializer=tf.contrib.layers.xavier_initializer(),
-                                                       trainable=self.is_training)
-                        #title encoding
-                        title_encoding = sentence_encoder(
-                                                       inputs=self.title_source,
-                                                       query=token_context,
-                                                       vocab_size=newsClsModelParams.source_vocab_size,
-                                                       multi_cnn_params=newsClsModelParams.token_cnn_params,#kernel,stride,layer
-                                                       scope='sentence',
-                                                       is_training=self.is_training,
-                                                       is_dropout=self.is_dropout,
-                                                       reuse=None) #N,FN
-                        #content encoding
-                        split_content,sentence_num = split_long_text(self.content_source,newsClsModelParams.title_maxlen)
-                        content_encoding = sentence_encoder(
-                                                       inputs=split_content,
-                                                       query=token_context,
-                                                       vocab_size=newsClsModelParams.source_vocab_size,
-                                                       multi_cnn_params=newsClsModelParams.token_cnn_params,#kernel,stride,layer
-                                                       scope='sentence',
-                                                       is_training=self.is_training,
-                                                       is_dropout=self.is_dropout,
-                                                       reuse=True)#N*ST,FN
-                        stack_content = stack_short_encode(content_encoding,sentence_num)#N,ST,FN
-			      #target to sentence embedding
-                        sentence_context = tf.get_variable('sentence_context',
-                                                       dtype=tf.float32,
-                                                       shape=[newsClsModelParams.target_vocab_size, embedLayerParams.embedding_dim],
-                                                       initializer=tf.contrib.layers.xavier_initializer(),
-                                                       trainable=self.is_training)
-
-                        content_encoding = doc_encoder(
-                                                       inputs=stack_content,
-                                                       query=sentence_context, 
-                                                       multi_cnn_params=newsClsModelParams.sentence_cnn_params,#kernel,stride,layer
-                                                       scope='doc',
-                                                       is_training=self.is_training,
-                                                       is_dropout=self.is_dropout,
-                                                       reuse=None) ##N,FN      
-                        #full_layer
-                        if newsClsModelParams.mode == 'content':
-                                    full_layer = content_encoding 
-                        elif newsClsModelParams.mode == 'title':    
-                                    full_layer = title_encoding      
- 
-                        self.logits = final_mlp_encoder(
-                                             inputs=full_layer,
-                                             output_dim=newsClsModelParams.target_vocab_size,
-                                             is_training=self.is_training,
-                                             is_dropout=self.is_dropout)
    
-class DirectAttenCls(multiClsModel):
+class AttenCls(multiClsModel):
             def _build_(self):
                         # input coding placeholder
                         self.title_source = tf.placeholder(shape=(None, newsClsModelParams.title_maxlen),dtype=tf.int64)
--- modules/text_encoder.py
+++ modules/text_encoder.py
@@ -15,7 +15,6 @@ def sentence_encoder(inputs,query,vocab_size,multi_cnn_params,scope,is_training,
                                    is_training=is_training,
                                    scope='embedding')
                 #convolution
-                
                 conv_out = multiLayer_conv_strip(
                                            inputs=embed,
                                            multi_cnn_params=multi_cnn_params,
@@ -28,32 +27,7 @@ def sentence_encoder(inputs,query,vocab_size,multi_cnn_params,scope,is_training,
                                            query=query,
                                            scope_name="target_atten",
                                            is_training=is_training,
-                                           is_dropout=is_dropout) #N,m,WD
+                                           is_dropout=is_dropout) #N,m,WD                 
+        return atten_layer
 
-                full_layer = conv_to_full_layer(
-                                           inputs=atten_layer,
-                                           scope_name="conv2full", 
-                                           is_training=is_training,
-                                           is_dropout=is_dropout)
-                 
-        return full_layer
-
-def doc_encoder(inputs,query,multi_cnn_params,scope,is_training,is_dropout,reuse):
-        with tf.variable_scope(scope,reuse=reuse):
-                #convolution
-                conv_out = multiLayer_conv_strip(
-                                           inputs=inputs,
-                                           multi_cnn_params=multi_cnn_params,
-                                           scope_name='ml_cnn',
-                                           is_training=is_training,
-                                           is_dropout=is_dropout)
-
-                atten_layer = target_attention(
-                                           inputs=conv_out,
-                                           query=query,
-                                           scope_name="target_atten",
-                                           is_training=is_training,
-                                           is_dropout=is_dropout) #N,m,WD
-
-                   
-        return full_layer
+ 
